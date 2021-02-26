b89b7320cb3beb83e86fc1a64a840e0c57752421
kitarp07 <imlaleet@gmail.com>
2021-02-26 14:14:15 +0545

    Initial commit

---
+++ arec..py
@@ -0,0 +1,7 @@
+#find the area of rectangle
+
+length = float(input ("enter the length:"))
+breadth = float(input ("enter the breadth"))
+
+area = length*breadth
+print (f'The area of rectangle is {area}')
